6c2574f912779be2781861cdf7fb3c074c93e858
Antony Cochet <antony.cochet@imagina.com>
2022-06-25 18:54:51 +0200

    get dept id and fetch dept/city coords

--- pages/[...detailMap].js
+++ pages/[...detailMap].js
@@ -1,13 +1,32 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
 
-export default function detailMap() {
-
-  const router = useRouter();
-
-  console.log(router.query.dept)
+export default function detailMap({dept,city}) {
 
   return (
     <p>DetailMap</p>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Fetch data from external API
+
+  let idDept;
+  if(context.query.dept){
+    idDept = context.query.dept
+  }
+
+  const resCity = await fetch(`https://france-geojson.gregoiredavid.fr/repo/departements/${idDept}/communes-${idDept}.geojson`)
+  const city = await resCity.json()
+
+  const resDept = await fetch(`https://france-geojson.gregoiredavid.fr/repo/departements/${idDept}/departement-${idDept}.geojson`)
+  const dept = await resDept.json()
+
+  // Pass data to the page via props
+  return { 
+    props: { 
+    city,
+    dept
+    } 
+  }
 }
